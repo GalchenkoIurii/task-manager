da2c9c37dea36faa54f52be8d0a22915a62f2f23
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-10-23 19:22:52 +0300

    clean some garbage from routes.php, init.php

--- config/init.php
+++ config/init.php
@@ -9,7 +9,6 @@ $app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
 $app_path = preg_replace("#[^/]+$#", '', $app_path);
 $app_path = str_replace('/public/', '', $app_path);
 define("PATH", $app_path);
-define("ADMIN", PATH . '/admin');
 
 define("ITEMS_PER_PAGE", 3);
 
--- config/routes.php
+++ config/routes.php
@@ -13,11 +13,6 @@ Router::addRoute('^add$', ['controller' => 'Main',
 Router::addRoute('^edit$', ['controller' => 'Main',
     'action' => 'edit']);
 
-//Router::addRoute('^admin$', ['controller' => 'Main',
-//    'action' => 'index', 'prefix' => 'admin']);
-//Router::addRoute('^admin/?(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$',
-//    ['prefix' => 'admin']);
-
 Router::addRoute('^$', ['controller' => 'Main',
     'action' => 'index']);
 Router::addRoute('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
